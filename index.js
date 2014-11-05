80ede30271586ef08de218cdb1471e3b2068d4bd
Snigdha Roy <snigdhar@andrew.cmu.edu>
2014-11-04 20:15:43 -0500

    Starting to figure out passport auth

--- index.js
+++ index.js
@@ -10,10 +10,54 @@ var knex = require('knex')({
 });
 
 var bookshelf = require('bookshelf')(knex);
-
 var User = bookshelf.Model.extend({
     tableName: 'users'
 });
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+// // Passport session setup.
+// // figure out how to findById alternative !!!!!
+// //   To support persistent login sessions, Passport needs to be able to
+// //   serialize users into and deserialize users out of the session.  Typically,
+// //   this will be as simple as storing the user ID when serializing, and finding
+// //   the user by ID when deserializing.
+// passport.serializeUser(function(user, done) {
+//   done(null, user.id);
+// });
+
+// passport.deserializeUser(function(id, done) {
+//   findById(id, function (err, user) {
+//     done(err, user);
+//   });
+// });
+
+
+
+// Use the LocalStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        // check also if the database had errors!
+        //  if (err) { return done(err); }
+        User.where({username: username}).fetch().then(function(user){
+            if (user === null) {
+                console.log("This username did not exist!");
+                return done(null, false, { message: 'Unknown user ' + username });
+            }
+            if (user.get('password') != password) {
+                return done(null, false, { message: 'Invalid password'});
+            }
+            return done(null, user);
+        });
+    })
+);
+
+
+
+
 
 // testing what we can do with knex and bookshelf
 knex.schema.dropTableIfExists('users').then(function() {
@@ -27,6 +71,10 @@ knex.schema.dropTableIfExists('users').then(function() {
     return User.forge({username:'delu',password:'yourmother'}).save().then(function(){
         console.log('created user "delu" with password "yourmother"');
     });
+// dummy authentication
+}).then(function () {
+
+
 // change user password
 }).then(function() {
     return User.where({username:'delu',password:'yourmother'}).fetch().then(function(model){
---
+++ resources.md
@@ -0,0 +1,3 @@
+Resources
+=========
+- (Passport setup)[https://github.com/jaredhanson/passport-local/blob/master/examples/login/app.js]
