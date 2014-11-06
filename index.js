bc05d163dbc2b390518bb9c2c9e2443d2fdfd5e0
Snigdha Roy <snigdhar@andrew.cmu.edu>
2014-11-05 20:19:43 -0500

    Cannot get print statements from passport.use to show up...

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function log(someString){
+    console.log(someString);
+}
+// function here(){
+//     var thisline = (new Error()).stack[1]
+//     log(thisline);
+// }
+
 var knex = require('knex')({
     client: 'pg',
     connection: {
@@ -16,21 +24,22 @@ var User = bookshelf.Model.extend({
 
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-// // Passport session setup.
-// // figure out how to findById alternative !!!!!
-// //   To support persistent login sessions, Passport needs to be able to
-// //   serialize users into and deserialize users out of the session.  Typically,
-// //   this will be as simple as storing the user ID when serializing, and finding
-// //   the user by ID when deserializing.
-// passport.serializeUser(function(user, done) {
-//   done(null, user.id);
-// });
+// Passport session setup.
+// figure out how to findById alternative !!!!!
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
 
-// passport.deserializeUser(function(id, done) {
-//   findById(id, function (err, user) {
-//     done(err, user);
-//   });
-// });
+passport.deserializeUser(function(id, done) {
+  User.where({username: username}).fetch().then(function(err, user){
+  //findById(id, function (err, user) {
+    done(err, user);
+  });
+});
 
 
 
@@ -41,20 +50,23 @@ var LocalStrategy = require('passport-local').Strategy;
 passport.use(new LocalStrategy(
     function(username, password, done) {
         // check also if the database had errors!
-        //  if (err) { return done(err); }
-        User.where({username: username}).fetch().then(function(user){
+        log("Trying to authenticate someone 1.");
+        User.where({username: username}).fetch().then(function(err, user){
+            if (err) { return done(err); }
+
             if (user === null) {
                 console.log("This username did not exist!");
                 return done(null, false, { message: 'Unknown user ' + username });
             }
             if (user.get('password') != password) {
+                log("Incorrect password.");
                 return done(null, false, { message: 'Invalid password'});
             }
+            log("Correct password! Logging you in!");
             return done(null, user);
         });
     })
 );
-
 
 
 
@@ -91,12 +103,12 @@ knex.schema.dropTableIfExists('users').then(function() {
 }).then(function() {
     return User.where({username:'delu'}).destroy();
 // check to see delu has been deleted
-}).then(function() {
-    return User.where({username:'delu'}).fetch().then(function(model){
-        if (model === null) {
-            console.log("delu has been destroyed");
-        }
-    });
+// }).then(function() {
+//     return User.where({username:'delu'}).fetch().then(function(model){
+//         if (model === null) {
+//             console.log("delu has been destroyed");
+//         }
+//     });
 // add other elements to users
 }).then(function() {
 
@@ -105,17 +117,51 @@ knex.schema.dropTableIfExists('users').then(function() {
     knex.schema.dropTable('users');
 });
 
+// Start the app / server
 var express = require('express');
 var app = express();
-app.set('bookshelf', bookshelf);
-
-app.get('/', function(req, res) {
-    res.send('<div style="color:red;">Hello World!</div>');
-});
-
 var server = app.listen(3000, function() {
     var host = server.address().address;
     var port = server.address().port;
 
     console.log('Example app listening at http://%s:%s', host, port);
 });
+
+// Start the database and authentication
+// app.configure(function() {
+    // ALL OF THESE NO LONGER BUNDLED WITH EXPRESS
+    // app.use(express.logger());
+    // app.use(express.cookieParser());
+    // app.use(express.bodyParser());
+    // app.use(express.methodOverride());
+    // app.use(express.session({ secret: 'supa secret homies!' }));
+    app.set('bookshelf', bookshelf);
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+    // app.use(app.router); // DEPRECATED
+
+// });
+
+
+
+// These are the urls we route to
+// req = request, res = response
+app.get('/', function(req, res) {
+    res.send('<div style="color:red;">Hello World!</div>');
+});
+app.get('/login', function(req,res){
+   res.sendFile(__dirname + '/auth.html');
+
+});
+app.get('/faillogin', function(req,res){
+   res.sendFile(__dirname + '/nologin.html');
+});
+app.post('/login', function(req, res){
+    log("User is: " + req.user.username);
+    passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/faillogin'})//,
+                                   // failureFlash: true })
+});
+
+
