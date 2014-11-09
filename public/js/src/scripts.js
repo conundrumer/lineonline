04c8367a84e98833ef4cf79763493d7df7b243f9
Jing Xiao <notajingoist@gmail.com>
2014-11-08 16:50:53 -0800

    added smooth scroll plugin for scroll chevron thing and used more icons; fixed up weird padding issues or something for the different panel sections

--- public/js/src/scripts.js
+++ public/js/src/scripts.js
@@ -1,1 +1,23 @@
-var $ = require('jquery');
+$ = jQuery = require('jquery');
+require('jquery-smooth-scroll');
+
+var LINEONLINE = {
+    init: function() {
+        this.initVars();
+        this.initPlugins();
+    },
+
+    initVars: function() {
+        this.navbarOffset = -70;
+        this.$scrollLink = $('.scroll-link');
+    },
+
+    initPlugins: function() {
+        this.$scrollLink.smoothScroll({
+            offset: this.navbarOffset,
+            speed: 600
+        });
+    }
+}
+
+LINEONLINE.init();
