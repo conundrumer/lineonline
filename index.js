f814311e42eb3aa4bd4ffec755a071abe6ca8743
Jing Xiao <notajingoist@gmail.com>
2014-11-09 22:14:51 -0800

    All: connected front end and backend for login and registration

--- index.js
+++ index.js
@@ -268,8 +268,8 @@ app.get('/faillogin-backend', function(req,res){
 // });
 
 app.post('/login-backend',
-    passport.authenticate('local', { successRedirect: '/-backend',
-                                   failureRedirect: '/faillogin-backend',
+    passport.authenticate('local', { successRedirect: '/home',
+                                   failureRedirect: '/signup-login',
                                    failureFlash: false })
 );
 app.post('/register-backend', register);
@@ -277,12 +277,12 @@ app.post('/edit-profile-backend', loginRequired, editProfile);
 
 app.get('/logout-backend', function(req, res){
   req.logout();
-  res.redirect('/-backend');
+  res.redirect('/signup-login');
 });
 
 function loginRequired(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login-backend');
+  res.redirect('/signup-login');
 }
 
 // example @login_required usage
@@ -304,42 +304,48 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public')); //serve static files
 
 //routes
-app.get('/', function(req, res) {
+app.get('/', loginRequired, function(req, res) {
     //res.send('<div style="color:red;">Hello World!</div>');
     res.render('index', { title: 'LineOnline' });
 });
 app.get('/signup-login', function(req, res) {
     res.render('signup-login', { title: 'Signup/Login', state: 'signedOut' });
 });
-app.get('/home', function(req, res) {
+app.get('/home', loginRequired, function(req, res) {
     res.render('home', { title: 'Home' });
 });
-app.get('/gallery', function(req, res) {
+app.get('/gallery', loginRequired, function(req, res) {
     res.render('gallery', { title: 'Gallery' });
 });
 app.get('/public-gallery', function(req, res) {
     res.render('public-gallery', { title: 'Gallery', state: 'signedOut' });
 });
-app.get('/your-tracks', function(req, res) {
+app.get('/your-tracks', loginRequired, function(req, res) {
     res.render('your-tracks', { title: 'Your Tracks' });
 });
-app.get('/profile', function(req, res) {
+app.get('/profile', loginRequired, function(req, res) {
     res.render('profile', { title: 'Profile' });
 });
-app.get('/favorites', function(req, res) {
+app.get('/favorites', loginRequired, function(req, res) {
     res.render('favorites', { title: 'Favorites' });
 });
-app.get('/subscriptions', function(req, res) {
+app.get('/subscriptions', loginRequired, function(req, res) {
     res.render('subscriptions', { title: 'Subscriptions' });
 });
-app.get('/settings', function(req, res) {
+app.get('/settings', loginRequired, function(req, res) {
     res.render('settings', { title: 'Settings' });
 });
+
+
+//actions
 app.get('/logout', function(req, res) {
-    res.redirect('/signup-login');
+    res.redirect('/logout-backend');
 });
-app.get('/login', function(req, res) {
-    res.redirect('/');
+app.post('/login', function(req, res) {
+    res.redirect(307, '/login-backend');
+});
+app.post('/signup', function(req, res) {
+    res.redirect(307, '/register-backend');
 });
 app.get('/search-public-gallery', function(req, res) {
     res.redirect('/public-gallery');
