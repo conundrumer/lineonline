ccf46ca13eba6d4e8823edf6f65bf5ac9c10a470
David Lu <davidelu93@gmail.com>
2014-11-21 03:18:06 -0500

    Add/reorganized unit tests

--- test/test.js
+++ test/test.js
@@ -2,11 +2,16 @@
 
 var demand = require('must');
 var express = require('express');
-var request = require('supertest');
+var request = require('supertest-as-promised');
+var Promise = require('bluebird');
 
 var url = 'http://localhost:3000/api';
 
 var users = require('./test_users');
+var dolan = users.dolan;
+var bob = users.bob;
+var cow = users.cow;
+var eve = users.eve;
 var track_ids = users.track_ids;
 var collection_ids = users.collection_ids;
 
@@ -17,9 +22,10 @@ var agent = {
     eve: request.agent(url)
 };
 
-describe('What a Single User Can Do', function () {
-    var dolan = users.dolan;
+// Begin with no data
 
+// registers dolan
+describe('Basic registration, authentication, and sessions (/auth)', function () {
     //REGISTER
     it('should be able to register (post: /auth/register)', function (done) {
         agent.dolan
@@ -36,35 +42,6 @@ describe('What a Single User Can Do', function () {
             .expect(200, dolan.user(), done);
     });
 
-    //GET USER DATA
-    it('should be able to get her id, username and null avatar_url (get: /users/:id)', function (done) {
-        agent.dolan
-            .get('/users/' + dolan.id)
-            .expect(200, dolan.user(), done);
-    });
-
-    //POST TRACK DATA
-    it('should be able to make a track (post: /tracks)', function (done) {
-        agent.dolan
-            .post('/tracks')
-            .send(dolan.unsaved_tracks()[0])
-            .expect(201, dolan.full_tracks()[0], done);
-    });
-
-    //GET ALL TRACK SNIPPET DATA
-    it('should be able to get her own track snippets (get: /users/:user_id/tracks)', function(done) {
-        agent.dolan
-            .get('/users/' + dolan.id + '/tracks')
-            .expect(200, dolan.track_snippets(), done);
-    });
-
-    //GET ONE FULL TRACK DATA
-    it('should be able to get all the data of her first track (get: /tracks/:track_id)', function(done) {
-        agent.dolan
-            .get('/tracks/' + track_ids.dolan[0])
-            .expect(200, dolan.full_tracks()[0], done);
-    });
-
     //LOG OUT
     it('should be able to log out (delete: /auth)', function(done) {
         agent.dolan
@@ -77,14 +54,6 @@ describe('What a Single User Can Do', function () {
         agent.dolan
             .get('/auth')
             .expect(401, done);
-    });
-
-    // CAN'T REGISTER SAME NAME
-    it('should not be able to register the same name (post: /auth/register)', function (done) {
-        agent.dolan
-            .post('/auth/register')
-            .send(dolan.registration())
-            .expect(400, done);
     });
 
     //LOG IN
@@ -101,8 +70,218 @@ describe('What a Single User Can Do', function () {
             .get('/auth')
             .expect(200, dolan.user(), done);
     });
+
+    // log out after testing
+    after(function(done) {
+        logout(agent.dolan).end(done);
+    });
 });
 
-// TODO: test subscriptions, favorites, empty collection
+function register(agent, user) {
+    return agent
+        .post('/auth/register')
+        .send(user.registration())
+        .expect(201, user.user());
+}
+function login(agent, user) {
+    return agent
+        .post('/auth')
+        .send(dolan.login())
+        .expect(200, user.user());
+}
+function logout(agent) {
+    return agent
+        .delete('/auth')
+        .expect(204);
+}
 
-// TODO: test for edge cases like registering w/ username that already exists, getting a user/track that doesn't exist, loginrequired, etc
+// registers bob
+describe('Basic user snippets (/users)', function () {
+    before(function () {
+        return register(agent.bob, bob);
+    });
+
+    it('should be able to get user snippets (get: /users/:id)', function (done) {
+        agent.dolan
+            .get('/users/' + dolan.id)
+            .expect(200, dolan.user())
+            .get('/users/' + bob.id)
+            .expect(200, bob.user(), done);
+    });
+
+    after(function (done) {
+        return logout(agent.bob);
+    });
+});
+
+describe('Basic track saving, full tracks, and track snippets (/tracks)', function () {
+
+    before(function() {
+        return new Promise.all([
+            login(agent.dolan, dolan),
+            login(agent.bob, bob)
+        ]);
+    });
+
+    it('should be able to make a track (post: /tracks)', function (done) {
+        agent.dolan
+            .post('/tracks')
+            .send(dolan.unsaved_tracks()[0])
+            .expect(201, dolan.full_tracks()[0], done);
+    });
+
+    it('should be able to make tracks (post: /tracks)', function (done) {
+        agent.bob
+            .post('/tracks')
+            .send(bob.unsaved_tracks()[0])
+            .expect(201, bob.full_tracks()[0])
+            .post('/tracks')
+            .send(bob.unsaved_tracks()[1])
+            .expect(201, bob.full_tracks()[1])
+            .then(done);
+    });
+
+    it('should be able to get her own track snippets (get: /users/:user_id/tracks)', function(done) {
+        return new Promise.all([
+            agent.dolan
+                .get('/users/' + dolan.id + '/tracks')
+                .expect(200, dolan.track_snippets()),
+            agent.bob
+                .get('/users/' + bob.id + '/tracks')
+                .expect(200, bob.track_snippets())
+        ]).then(done);
+    });
+
+    it('should be able to get other user\'s track snippets (get: /users/:user_id/tracks)', function(done) {
+        return new Promise.all([
+            agent.bob
+                .get('/users/' + dolan.id + '/tracks')
+                .expect(200, dolan.track_snippets()),
+            agent.dolan
+                .get('/users/' + bob.id + '/tracks')
+                .expect(200, bob.track_snippets())
+        ]).then(done);
+    });
+
+    it('should be able to get a full track (get: /tracks/:track_id)', function(done) {
+        agent.dolan
+            .get('/tracks/' + track_ids.dolan[0])
+            .expect(200, dolan.full_tracks()[0], done);
+    });
+
+    it('should be able to get more full tracks (get: /tracks/:track_id)', function(done) {
+        agent.bob
+            .get('/tracks/' + track_ids.bob[0])
+            .expect(200, bob.full_tracks()[0])
+            .get('/tracks/' + track_ids.bob[1])
+            .expect(200, bob.full_tracks()[1], done);
+    });
+
+    // but dolan isn't a collaborator on either of bob's tracks
+    // this will need to change once we figure out playback_tracks
+    it('should be able to get someone else\'s full tracks (get: /tracks/:track_id)', function(done) {
+        agent.dolan
+            .get('/tracks/' + track_ids.bob[0])
+            .expect(200, bob.full_tracks()[0])
+            .get('/tracks/' + track_ids.bob[1])
+            .expect(200, bob.full_tracks()[1], done);
+    });
+
+    after(function() {
+        return new Promise.all([
+            logout(agent.dolan),
+            logout(agent.bob)
+        ]);
+    });
+});
+
+describe('Invalid registration and authentication', function () {
+    // define the error message the way want them to be defined pls
+    var errors = {
+        some_fields_empty: { message: 'some fields are empty' },
+        username_already_exists: { message: 'username already exists' },
+        email_already_exists: { message: 'email already exists' },
+        invalid_login: { message: 'invalid login' }
+    };
+
+    var dolan_reg = dolan.registration();
+    var bob_reg = bob.registration();
+    var dolan_login = dolan.login();
+    var bob_login = bob.login();
+
+    it('should not be able to register with some empty fields (post: /auth/register)', function (done) {
+        var reg = {
+            username: '',
+            email: '',
+            password: ''
+        };
+        agent.dolan
+            .post('/auth/register')
+            .send(reg)
+            .expect(400, errors.some_fields_empty, done);
+    });
+
+    it('should not be able to register with a username that already exists (post: /auth/register)', function (done) {
+        var reg = {
+            username: dolan_reg.username,
+            email: bob_reg.email,
+            password: bob_reg.password
+        };
+        agent.dolan
+            .post('/auth/register')
+            .send(reg)
+            .expect(400, errors.username_already_exists, done);
+    });
+
+    it('should not be able to register with an email that already exists (post: /auth/register)', function (done) {
+        var reg = {
+            username: bob_reg.username,
+            email: dolan_reg.email,
+            password: bob_reg.password
+        };
+        agent.dolan
+            .post('/auth/register')
+            .send(dolan.registration())
+            .expect(400, errors.email_already_exists,done);
+    });
+
+    it('should not be able to log in with a non-existent username (post: /auth/register)', function (done) {
+        var login = {
+            username: 'void',
+            password: 'void'
+        };
+        agent.dolan
+            .post('/auth')
+            .send(login)
+            .expect(401, errors.invalid_login, done);
+    });
+
+    it('should not be able to log in with the wrong password (post: /auth/register)', function (done) {
+        var login = {
+            username: dolan_login.username,
+            password: bob_login.password
+        };
+        agent.dolan
+            .post('/auth')
+            .send(login)
+            .expect(401, errors.invalid_login, done);
+    });
+
+    // can a logged-in user do any of the following: login, register?
+    // can a logged-out user logout again?
+});
+
+// TODO: split tests to separate files before it gets too long
+
+// TODO: profile
+// TODO: gallery
+// TODO: subscriptions
+// TODO: favorites
+// TODO: collections
+
+// TODO: invitations
+// TODO: collaboration (non-realtime)
+// TODO: privacy
+// TODO: conversations (non-realtime)
+
+// TODO: edge cases?
