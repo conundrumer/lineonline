112b24d078edad93017d62792f1137b2c9661449
David Lu <davidelu93@gmail.com>
2014-11-21 16:38:41 -0500

    Pass tracks-basic tests

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -1,34 +1,118 @@
+var Promise = require('bluebird');
+
 var User = require('../models/user');
 var Track = require('../models/track');
 
-function getFullTrackInfo(model){
-    return {
-        'id': model.get('id'),
-        'owner': model.get('owner'),
-        'title': model.get('title'),
-        'description': model.get('description'),
-        'collaborators': [],
-        'invites': [],
-        'tags': []
+// TODO: don't repeat yourself
+function getUser(user_id) {
+    return User.where({id: user_id}).fetch();
+}
+function getUserSnippetJSON(user_id) {
+    return getUser(user_id)
+        .then(function (model) {
+            return {
+                user_id: model.get('id'),
+                username: model.get('username'),
+                avatar_url: model.get('avatar_url')
+            };
+        });
+}
+
+function getFullTrackJSON(model, ownerSnippet){
+    var track = {
+        track_id: model.get('id'),
+        scene: model.get('scene'),
+        title: model.get('title'),
+        description: model.get('description'),
+        owner: ownerSnippet,
+        preview: {
+            top: model.get('preview_top'),
+            left: model.get('preview_left'),
+            bottom: model.get('preview_bottom'),
+            right: model.get('preview_right'),
+        },
+        collaborators: [],
+        invitees: [],
+        time_created: '',
+        time_modified: '',
+        tags: [],
+        conversation: {
+            messages: []
+        }
     };
+    if (ownerSnippet) {
+        return new Promise.resolve(track);
+    }
+    return getUserSnippetJSON(model.get('owner'))
+        .then(function(ownerSnippet) {
+            track.owner = ownerSnippet;
+            return track;
+        });
+}
+
+function getTrackSnippetJSON(model, ownerSnippet) {
+    var trackSnippet = {
+        track_id: model.get('id'),
+        scene: model.get('scene'),
+        title: model.get('title'),
+        description: model.get('description'),
+        owner: ownerSnippet,
+        preview: {
+            top: model.get('preview_top'),
+            left: model.get('preview_left'),
+            bottom: model.get('preview_bottom'),
+            right: model.get('preview_right'),
+        }
+    };
+    if (ownerSnippet) {
+        return new Promise.resolve(trackSnippet);
+    }
+    return getUserSnippetJSON(model.get('owner'))
+        .then(function(ownerSnippet) {
+            track.owner = ownerSnippet;
+            return track;
+        });
 }
 
 exports.makeTrack = function(req, res) {
+    var track = req.body;
     Track.forge({
-       title: req.body.title,
-       description: req.body.description,
-       owner: req.user.get('id')
-    }).save().then(function(model){
-        res.status(201).json(getFullTrackInfo(model));
-    });
+        scene: track.scene,
+        title: track.title,
+        description: track.description,
+        owner: req.user.get('id'),
+        preview_top: track.preview.top,
+        preview_left: track.preview.left,
+        preview_bottom: track.preview.bottom,
+        preview_right: track.preview.right
+    }).save()
+    .then(getFullTrackJSON)
+    .then(function(track) {
+        res.status(201).json(track);
+    }).catch(console.error);
 };
 
 exports.getTrack = function(req, res) {
     Track.where({id: req.params.track_id}).fetch()
-    .then (function (model) {
-        if (!model) {
-            res.status(404).send();
-        }
-        res.json(getFullTrackInfo(model));
+    .then(getFullTrackJSON)
+    .then(function(track) {
+        res.status(200).json(track);
+    });
+};
+
+exports.getUserTracks = function(req, res) {
+    var user_id = req.params.id;
+    new Promise.all([
+        getUserSnippetJSON(user_id),
+        Track.where({owner: user_id}).fetchAll()
+    ]).then(function(values) {
+        var userSnippet = values[0];
+        var trackModels = values[1];
+        new Promise.all(trackModels.map(function(model) {
+            return getTrackSnippetJSON(model, userSnippet);
+        })).then(function(tracks) {
+            res.status(200).json(tracks);
+        });
+
     });
 };
--- models/track.js
+++ models/track.js
@@ -1,5 +1,6 @@
 var User = require('./user').model;
 
+var USE_JSONB = false;
 var Track = require('../db/create-model')({
     tableName: 'tracks',
     build: function (table) {
@@ -7,6 +8,11 @@ var Track = require('../db/create-model')({
         table.string('title', 100);
         table.string('description', 100);
         table.integer('owner').references('users.id');
+        table.json('scene', USE_JSONB);
+        table.float('preview_top');
+        table.float('preview_left');
+        table.float('preview_bottom');
+        table.float('preview_right');
     },
     owner: function(){
         return this.belongsTo(User, 'owner');
