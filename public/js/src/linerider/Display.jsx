e7a0d12f1d7816d97c002a26c70eaa0cd7cd55c3
David Lu <davidelu93@gmail.com>
2014-11-24 03:08:20 -0500

    Refluxified line rider editor

--- public/js/src/linerider/Display.jsx
+++ public/js/src/linerider/Display.jsx
@@ -1,4 +1,5 @@
 var React = require('react/addons');
+var _ = require('underscore');
 
 var fullSize = {
     position: 'absolute',
@@ -17,10 +18,10 @@ var Line = React.createClass({
     render: function() {
         return (
             <line
-                x1={this.props.line.p1.x}
-                y1={this.props.line.p1.y}
-                x2={this.props.line.p2.x}
-                y2={this.props.line.p2.y}
+                x1={this.props.p1.x}
+                y1={this.props.p1.y}
+                x2={this.props.p2.x}
+                y2={this.props.p2.y}
                 stroke={this.props.color}
                 strokeWidth='4'
                 strokeLinecap='round'
@@ -51,15 +52,23 @@ var Display = React.createClass({
         window.removeEventListener('resize', this.onResize);
     },
     render: function() {
+        var scene = this.props.scene;
         return (
             <div ref='container' style={fullSize}>
                 <svg width={this.state.width} height={this.state.height}>
-                    {this.props.lines.map(function(line) {
-                        return <Line line={line} />;
-                    })}
+                    { _.pairs(scene.lines).map(function(pair) {
+                        var key = pair[0];
+                        var points = pair[1];
+                        var p1 = scene.points[points.p1];
+                        var p2 = scene.points[points.p2];
+                        return <Line key={key} p1={p1} p2={p2} />;
+                    }.bind(this)) }
                     { this.props.drawingLine ?
-                        <Line line={this.props.drawingLine} color='red' /> :
-                        null
+                        <Line key={-1}
+                            p1={this.props.drawingLine.p1}
+                            p2={this.props.drawingLine.p2}
+                            color='red'
+                        /> : null
                     }
                 </svg>
             </div>
---
+++ public/js/src/linerider/store.js
@@ -0,0 +1,96 @@
+var Reflux = require('reflux');
+var _ = require('underscore');
+var EditorActions = require('./actions');
+var ERASER_RADIUS = 5;
+
+// ugh i neeed to make a vector class
+function distance(p1, p2) {
+    var dx = p1.x - p2.x;
+    var dy = p1.y - p2.y;
+    return Math.sqrt(dx*dx + dy*dy);
+}
+
+function length(v) {
+    return Math.sqrt(v.x * v.x + v.y * v.y);
+}
+
+function rotate(p, a) {
+    return {
+        x: Math.cos(a)*p.x - Math.sin(a)*p.y,
+        y: Math.sin(a)*p.x + Math.cos(a)*p.y
+    };
+}
+
+// gonna use trig functions because i do't want to deal w/ linear algebra
+function lineSegmentDistance(p, line) {
+    // make everything relative to p, ie move things so p is at (0,0)
+    var p1 = {
+        x: line.p1.x - p.x,
+        y: line.p1.y - p.y
+    };
+    var p2 = {
+        x: line.p2.x - p.x,
+        y: line.p2.y - p.y
+    };
+    // orient line to be perpedicular to x
+    var lineAngle = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+    p1 = rotate(p1, -lineAngle);
+    p2 = rotate(p2, -lineAngle);
+    if ((p1.x < 0 && 0 < p2.x) || (p2.x < 0 && 0 < p1.x)) {
+        return Math.abs(p1.y);
+    } else {
+        return Math.min(length(p1), length(p2));
+    }
+}
+
+var SceneStore = Reflux.createStore({
+    listenables: [EditorActions],
+    getDefaultData: function() {
+        this.scene = {
+            next_point_id: 0,
+            next_line_id: 0,
+            points:{},
+            lines:{}
+        };
+        return this.scene;
+    },
+    onNewScene: function () {
+        this.trigger(this.getDefaultData(), 'new scene');
+    },
+    onLoadScene: function (scene) {
+        this.scene = scene;
+        this.trigger(scene, 'load scene');
+    },
+    // editing functions
+    // no snapping but stuff will get more complicated when that's implemented
+    onAddLine: function (p1, p2) {
+        var scene = this.scene;
+        var pointID = scene.next_point_id;
+        var lineID = scene.next_line_id;
+        // no correction for pan/zoom
+        scene.points[pointID] = p1;
+        scene.points[pointID+1] = p2;
+        scene.lines[lineID] = { p1: pointID, p2: pointID+1 };
+        scene.next_point_id = pointID + 2;
+        scene.next_line_id = lineID + 1;
+        this.trigger(scene, 'add line');
+    },
+    onRemoveLines: function (pos) {
+        var scene = this.scene;
+        _.keys(scene.lines).filter(function(id) {
+            var line = scene.lines[id];
+            var p1 = scene.points[line.p1];
+            var p2 = scene.points[line.p2];
+            // console.log(lineSegmentDistance(pos, {p1: p1, p2: p2}))
+            return lineSegmentDistance(pos, {p1: p1, p2: p2}) < ERASER_RADIUS;
+        }).forEach(function(id) {
+            var line = scene.lines[id];
+            delete scene.points[line.p1];
+            delete scene.points[line.p2];
+            delete scene.lines[id];
+        }.bind(this));
+        this.trigger(scene, 'remove lines');
+    }
+});
+
+module.exports = SceneStore;
