eac60c56525174f4b86c3988f4407dad2099b3d9
David Lu <davidelu93@gmail.com>
2014-11-30 00:34:51 -0500

    Add starter code for user invitaitons

---
+++ controllers/invitations.js
@@ -0,0 +1,12 @@
+
+exports.getInvitations = function(req, res) {
+    res.status(501).send();
+};
+
+exports.accept = function(req, res) {
+    res.status(501).send();
+};
+
+exports.decline = function(req, res) {
+    res.status(501).send();
+};
