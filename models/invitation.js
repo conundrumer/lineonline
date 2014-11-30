6628ec19ea442a38d6e737ce41d260d894fafa36
David Lu <davidelu93@gmail.com>
2014-11-30 00:46:01 -0500

    Implement invitation model

--- models/invitation.js
+++ models/invitation.js
@@ -4,10 +4,18 @@ var Track = require('./track');
 
 var Invitation = bookshelf.Model.extend({
     tableName: 'invitation',
+    track: function() {
+        return this.belongsTo(Track);
+    },
+    invitee: function() {
+        return this.belongsTo(User, 'invitee');
+    }
 }, {
     tableName: 'invitation',
     build: function (table) {
         table.increments('id').primary();
+        table.integer('track').references('tracks.id');
+        table.integer('invitee').references('users.id');
     }
 });
 
