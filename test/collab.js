a275a61b8cfe2e484c8372b740e4b9016b1212c0
David Lu <davidelu93@gmail.com>
2014-11-30 16:23:43 -0500

    Add idempotence testts

--- test/collab.js
+++ test/collab.js
@@ -115,4 +115,106 @@ describe('Collaboration: A user', function () {
             .catch(done);
     });
 
+    // idempotence
+
+    it('should be able to accept an invitation multiple times with no effect (put: /invitations/:track_id)', function (done) {
+        agent.bob // bob invite dolan
+            .put('/tracks/' + track_ids.bob[0] + '/invitations/' + dolan.id)
+            .then(function() {
+                return agent.dolan // dolan accept bob's invitation
+                    .put('/invitations/' + track_ids.bob[0])
+                    .expect(StatusTypes.noContent);
+            })
+            .then(function() {
+                return agent.dolan // dolan accept bob's invitation again
+                    .put('/invitations/' + track_ids.bob[0])
+                    .expect(StatusTypes.noContent);
+            })
+            .then(function() {
+                return agent.dolan // dolan see one of his collabs
+                    .get('/collaborations')
+                    .expect(StatusTypes.noContent, [bob.track_snippets()[0]])
+            })
+            .then(function() {
+                return agent.bob // see track's 1 collaborators
+                    .get('/tracks/' + track_ids.bob[0] + '/collaborators')
+                    .expect(StatusTypes.ok, [dolan.user()]);
+            })
+            .then(function() {
+                var updatedTrack = bob.full_tracks()[0];
+                updatedTrack.collaborators = [dolan.user()];
+                return agent.bob // see track with collaborators
+                    .get('/tracks/' + track_ids.bob[0])
+                    .expect(StatusTypes.ok, updatedTrack);
+            })
+            .then(function() {done();})
+            .catch(done);
+    });
+
+    it('should be able to leave a collab multiple times with no effect (delete: /collaborations/:track_id)', function (done) {
+        agent.dolan // leave collab
+            .delete('/collaborations/' + track_ids.bob[0])
+            .expect(StatusTypes.noContent)
+            .then(function() {
+                return agent.dolan // leave collab AGAIN
+                    .delete('/collaborations/' + track_ids.bob[0])
+                    .expect(StatusTypes.noContent);
+            })
+            .then(function() {
+                return agent.dolan // see no collab
+                    .get('/collaborations')
+                    .expect(StatusTypes.noContent);
+            })
+            .then(function() {
+                return agent.bob // see no colalbroatrs on track
+                    .get('/tracks/' + track_ids.bob[0] + '/collaborators')
+                    .expect(StatusTypes.ok, []);
+            })
+            .then(function() {
+                return agent.bob // see track with no collabs
+                    .get('/tracks/' + track_ids.bob[0])
+                    .expect(StatusTypes.ok, bob.full_tracks()[0]);
+            })
+            .then(function() {done();})
+            .catch(done);
+    });
+
+
+    it('should be able to remove a collaborator multiple times with no effect (delete: /tracks/:track_id/collaborators/:user_id)', function (done) {
+        agent.bob // invite dolan
+            .put('/tracks/' + track_ids.bob[0] + '/invitations/' + dolan.id)
+            .then(function() {
+                return agent.dolan // accept invitation
+                    .put('/invitations/' + track_ids.bob[0])
+                    .expect(StatusTypes.noContent);
+            })
+            .then(function() {
+                return agent.bob // remove dolan
+                    .delete('/tracks/' + track_ids.bob[0] + '/collaborators/' + dolan.id)
+                    .expect(StatusTypes.noContent);
+            })
+            .then(function() {
+                return agent.bob // remove dolan again
+                    .delete('/tracks/' + track_ids.bob[0] + '/collaborators/' + dolan.id)
+                    .expect(StatusTypes.noContent);
+            })
+            .then(function() {
+                return agent.dolan // see no collab
+                    .get('/collaborations')
+                    .expect(StatusTypes.noContent);
+            })
+            .then(function() {
+                return agent.bob // see no collab on track
+                    .get('/tracks/' + track_ids.bob[0] + '/collaborators')
+                    .expect(StatusTypes.ok, []);
+            })
+            .then(function() {
+                return agent.bob // track has no collab
+                    .get('/tracks/' + track_ids.bob[0])
+                    .expect(StatusTypes.ok, bob.full_tracks()[0]);
+            })
+            .then(function() {done();})
+            .catch(done);
+    });
+
 });
