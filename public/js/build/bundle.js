97c4414c669d95adce0ca295592719ef1b5600e4
Jing Xiao <notajingoist@gmail.com>
2014-12-04 17:19:10 -0500

    made real tracks slider showing max 3 tracks for subscriptions

--- public/js/build/bundle.js
+++ public/js/build/bundle.js
@@ -52736,37 +52736,42 @@ module.exports = TracksPreview;
 var React = require('react/addons');
 
 //UI Components
-var Tile = require('./Tile.jsx');
+var TracksCol = require('./TracksCol.jsx');
 
 var TracksSlider = React.createClass({displayName: 'TracksSlider',
     render: function() {
+        console.log(this.props.tracks);
+        var tracks = [];
+        var len = (this.props.tracks.length >= 3) ? 3 : this.props.tracks.length;
+        for (var i = 0; i < len; i++) {
+            tracks.push(this.props.tracks[i]);
+        }
+
+        var trackCols = tracks.map(function(track, idx, arr) {
+            var col;
+            if (idx === '0') {
+                col = 'col-first';
+            } else if (idx === '1') {
+                col = 'col-second';
+            } else {
+                col = 'col-third';
+            }
+            return (
+                React.createElement(TracksCol, {
+                    col: col, 
+                    userId: this.props.userId, 
+                    tracks: [track]}
+                )
+            );
+        }.bind(this));
+
         //this.props.tracks
         return (
             React.createElement("div", {className: "col span_3_of_4"}, 
-                React.createElement("div", {className: "gallery-col col span_1_of_3"}, 
-                    React.createElement(Tile, {
-                        title: "Track Title", 
-                        description: "Description 1 description blah blah blah", 
-                        col: "col-first", 
-                        userId: this.props.userId}
-                    )
-                ), 
-                React.createElement("div", {className: "gallery-col col span_1_of_3"}, 
-                    React.createElement(Tile, {
-                        title: "Track Title", 
-                        description: "Description 1 description blah blah blah", 
-                        col: "col-first", 
-                        userId: this.props.userId}
-                    )
-                ), 
-                React.createElement("div", {className: "gallery-col col span_1_of_3"}, 
-                    React.createElement(Tile, {
-                        title: "Track Title", 
-                        description: "Description 1 description blah blah blah", 
-                        col: "col-first", 
-                        userId: this.props.userId}
-                    )
-                )
+                this.props.tracks && this.props.tracks.length > 0 ?
+                    trackCols
+                    : null
+                
             )
 
         );
@@ -52775,7 +52780,7 @@ var TracksSlider = React.createClass({displayName: 'TracksSlider',
 
 module.exports = TracksSlider;
 
-},{"./Tile.jsx":"/Users/jingxiao/437/Team77/public/js/src/ui/Tile.jsx","react/addons":"/Users/jingxiao/437/Team77/node_modules/react/addons.js"}],"util/status-types.js":[function(require,module,exports){
+},{"./TracksCol.jsx":"/Users/jingxiao/437/Team77/public/js/src/ui/TracksCol.jsx","react/addons":"/Users/jingxiao/437/Team77/node_modules/react/addons.js"}],"util/status-types.js":[function(require,module,exports){
 var StatusTypes = {
     info: 100,
     ok: 200,
