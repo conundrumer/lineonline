bc71d7c7aa8627897927309a735617c2975fb208
Jing Xiao <notajingoist@gmail.com>
2014-12-04 01:01:52 -0500

    deleted username password stuff for pg config

--- db/bookshelf.dev.js
+++ db/bookshelf.dev.js
@@ -5,9 +5,18 @@ module.exports = require('bookshelf')(require('knex')({
     client: 'pg',
     connection: {
         host: process.env.DATABASE_URL || 'localhost',
-        user: 'jpgilioziufjiz',
-        password: 'e39023z34sJVYIPQYb8U5hgN0j',
-        database: 'd53dme13on6ei9',
         charset: 'utf8'
     }
 })).plugin('registry');
+
+// module.exports = require('bookshelf')(require('knex')({
+//     client: 'pg',
+//     connection: {
+//         host: 'localhost',
+//         user: 'webapps',
+//         password: 'fun',
+//         database: 'lineonline',
+//         charset: 'utf8'
+//     }
+// })).plugin('registry');
+
