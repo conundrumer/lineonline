7300f689d716d0c20e0a616c1d157346a5105baa
Jing Xiao <notajingoist@gmail.com>
2014-12-04 00:43:48 -0500

    changed database setttings for heroku

--- db/bookshelf.dev.js
+++ db/bookshelf.dev.js
@@ -1,11 +1,13 @@
 // a bookshelf instance using the dev db settings
+// var pg = require('pg');
+
 module.exports = require('bookshelf')(require('knex')({
     client: 'pg',
     connection: {
-        host: 'localhost',
-        user: 'webapps',
-        password: 'fun',
-        database: 'lineonline',
+        host: process.env.DATABASE_URL || 'localhost',
+        user: 'jpgilioziufjiz',
+        password: 'e39023z34sJVYIPQYb8U5hgN0j',
+        database: 'd53dme13on6ei9',
         charset: 'utf8'
     }
 })).plugin('registry');
