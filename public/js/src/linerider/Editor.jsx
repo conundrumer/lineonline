4af1055c235882606114978e9788acf26acc8aca
David Lu <davidelu93@gmail.com>
2014-12-07 02:20:14 -0500

    Refactor editing to use Kefir + separate tools to tools.js + fix mouse offset issue

--- public/js/src/linerider/Editor.jsx
+++ public/js/src/linerider/Editor.jsx
@@ -1,5 +1,6 @@
 var React = require('react/addons');
 var Reflux = require('reflux');
+var _ = require('underscore');
 
 var Actions = require('./actions');
 var ErrorActions = require('../actions-error');
@@ -7,30 +8,15 @@ var sceneStore = require('./store');
 
 var Display = require('./Display.jsx');
 var Icon = require('../ui/Icon.jsx');
-var _ = require('underscore');
-
-var TOOL = {
-    LINE: 1,
-    ERASE: 2,
-    PENCIL: 3
-};
 
 var toolbarStyle = {
     position: 'absolute',
 
 };
 
-function distance(p1, p2) {
-    var dx = p1.x - p2.x;
-    var dy = p1.y - p2.y;
-    return Math.sqrt(dx*dx + dy*dy);
-}
-
-var MIN_LINE_LENGTH = 10;
-
 var Editor = React.createClass({
-    navbarOffset: 70,
     mixins: [
+        require('./tools'),
         Reflux.listenTo(sceneStore, 'onSceneChanged')
     ],
     onSceneChanged: function(data, action) {
@@ -46,25 +32,16 @@ var Editor = React.createClass({
     getInitialState: function() {
         return {
             scene: sceneStore.getDefaultData(),
-            editState: TOOL.PENCIL,
+            toolHandler: this.pencil,
             startPos: null,
             movePos: null
         };
     },
-    componentWillMount: function() {
-    },
-    componentWillUnmount: function() {
-        window.removeEventListener('mousemove', this.onMouseMove);
-        window.removeEventListener('mouseup', this.onMouseUp);
-    },
-    componentWillReceiveProps: function(nextProps) {
-    },
-    // mouse handlers
-    onToolClick: function(editState) {
+    onToolClick: function(tool) {
         return function onClick(e) {
             e.preventDefault();
             this.setState({
-                editState: editState
+                toolHandler: tool
             });
         }.bind(this);
     },
@@ -84,61 +61,6 @@ var Editor = React.createClass({
         }
 
     },
-    // not sure how reliable it is in getting the right position
-    // will refactor to use RxJS when editing gets more complex
-    onMouseDown: function(e) {
-        e.preventDefault();
-        window.addEventListener('mousemove', this.onMouseMove);
-        window.addEventListener('mouseup', this.onMouseUp);
-        var startPos = {
-            x: e.pageX,
-            y: e.pageY - this.navbarOffset
-        };
-        switch (this.state.editState) {
-            case TOOL.ERASE:
-                Actions.eraseLines(startPos);
-                break;
-        }
-        this.setState({ startPos: startPos });
-    },
-    onMouseMove: function(e) {
-        e.preventDefault();
-        var startPos = this.state.startPos;
-        var movePos = {
-            x: e.pageX,
-            y: e.pageY - this.navbarOffset
-        };
-        switch (this.state.editState) {
-            case TOOL.PENCIL:
-                if (distance(startPos, movePos) > MIN_LINE_LENGTH) {
-                    Actions.drawLine(this.props.userID, startPos, movePos);
-                    this.setState({ startPos: movePos });
-                }
-                break;
-            case TOOL.ERASE:
-                Actions.eraseLines(movePos);
-                break;
-        }
-        this.setState({ movePos: movePos });
-    },
-    onMouseUp: function(e) {
-        e.preventDefault();
-        window.removeEventListener('mousemove', this.onMouseMove);
-        window.removeEventListener('mouseup', this.onMouseUp);
-        var startPos = this.state.startPos;
-        var endPos = {
-            x: e.pageX,
-            y: e.pageY - this.navbarOffset
-        };
-        switch (this.state.editState) {
-            case TOOL.LINE:
-                if (distance(startPos, endPos) > MIN_LINE_LENGTH) {
-                    Actions.drawLine(this.props.userID, startPos, endPos);
-                }
-                break;
-        }
-        this.setState({ startPos: null, movePos: null });
-    },
     onToolBarMouseDown: function(e) {
         e.stopPropagation();
     },
@@ -150,27 +72,22 @@ var Editor = React.createClass({
         var drawingLine;
         var startPos = this.state.startPos;
         var movePos = this.state.movePos;
-        switch (this.state.editState) {
-            case TOOL.LINE:
-            case TOOL.PENCIL:
-                if (startPos && movePos) {
-                    drawingLine = {
-                        p1: startPos,
-                        p2: movePos
-                    };
-                }
-                break;
+        if (startPos && movePos) {
+            drawingLine = {
+                p1: startPos,
+                p2: movePos
+            };
         }
         return (
-            <div onMouseDown={this.onMouseDown}>
+            <div ref='canvas' onMouseDown={this.startTool}>
                 <Display
                     drawingLine={drawingLine}
                     scene={this.state.scene}
                 />
                 <div onMouseDown={this.onToolBarMouseDown} className='toolbar'>
-                    <ToolButton onClick={this.onToolClick(TOOL.PENCIL)} icon='pencil' name='Pencil' />
-                    <ToolButton onClick={this.onToolClick(TOOL.LINE)} icon='minus' name='Line' />
-                    <ToolButton onClick={this.onToolClick(TOOL.ERASE)} icon='delete' name='Erase' />
+                    <ToolButton onClick={this.onToolClick(this.pencil)} icon='pencil' name='Pencil' />
+                    <ToolButton onClick={this.onToolClick(this.line)} icon='minus' name='Line' />
+                    <ToolButton onClick={this.onToolClick(this.eraser)} icon='delete' name='Erase' />
                     <ToolButton
                         onClick={this.onSaveSetting}
                         icon={ this.props.isNewTrack ? 'check' : 'cog' }
---
+++ public/js/src/linerider/tools.js
@@ -0,0 +1,124 @@
+var Kefir = require('kefir');
+var Actions = require('./actions');
+
+function distance(p1, p2) {
+    var dx = p1.x - p2.x;
+    var dy = p1.y - p2.y;
+    return Math.sqrt(dx*dx + dy*dy);
+}
+var MIN_LINE_LENGTH = 10;
+
+var ToolsMixin = {
+    startTool: function(e) {
+        this.mouseDown.emit(e);
+    },
+    // lifecycle methods
+    componentWillMount: function() {
+        this.mouseDown = Kefir.emitter();
+        this.mouseMove = Kefir.fromEvent(window, 'mousemove')
+            .map(this.mouseCoords);
+        this.mouseUp = Kefir.fromEvent(window, 'mouseup')
+            .map(this.mouseCoords);
+        this.toolStream = this.mouseDown
+            .map(this.mouseCoords);
+
+        this.toolStream.onValue(this.onTool);
+    },
+    componentWillUnmount: function() {
+        this.unsubscribeTool();
+        this.toolStream.offValue(this.onTool);
+    },
+    // tool subscription management
+    onTool: function(start) {
+        this.state.toolHandler(start, this.mouseMove, this.mouseUp);
+    },
+    subscribeTool: function(tool) {
+        stream = tool.stream.takeUntilBy(this.mouseUp);
+        onValue = tool.onValue.bind(this);
+        onEnd = tool.onEnd && tool.onEnd.bind(this) || function() {};
+        function unsubscribeTool () {
+            stream.offValue(onValue);
+            stream.offEnd(endTool);
+        }
+        function endTool() {
+            onEnd();
+            unsubscribeTool();
+        }
+        stream.onValue(onValue);
+        stream.onEnd(endTool);
+        this.unsubscribeTool = unsubscribeTool;
+    },
+    unsubscribeTool: function(){},
+    // util
+    mouseCoords: function(e) {
+        e.preventDefault();
+        var rect = this.refs.canvas.getDOMNode().getBoundingClientRect();
+        return {
+            x: e.pageX - rect.left,
+            y: e.pageY - rect.top
+        };
+    },
+    // tool definitions
+    pencil: function(start, moveStream) {
+        var setState = this.setState.bind(this);
+        setState({ startPos: start });
+
+        var stream = moveStream
+            .withDefault(start)
+            .tap(function(p) {
+                setState({ movePos: p });
+            })
+            .scan(function(prev, next) {
+                return (distance(prev, next) > MIN_LINE_LENGTH) ? next : prev;
+            })
+            .skipDuplicates()
+            .tap(function(p) {
+                setState({ startPos: p });
+            })
+            .slidingWindow(2,2);
+
+        this.subscribeTool({
+            stream: stream,
+            onValue: function (line) {
+                Actions.drawLine(this.props.userID, line[0], line[1]);
+            },
+            onEnd: function () {
+                this.setState({ startPos: null, movePos: null });
+            }
+        });
+    },
+    line: function(start, moveStream) {
+        this.setState({ startPos: start });
+        var end = start;
+
+        this.subscribeTool({
+            stream: moveStream,
+            onValue: function (p) {
+                this.setState({ movePos: p });
+                end = p;
+            },
+            onEnd: function () {
+                this.setState({ startPos: null, movePos: null });
+                if (distance(start, end) > MIN_LINE_LENGTH) {
+                    Actions.drawLine(this.props.userID, start, end);
+                }
+            }
+        });
+    },
+    eraser: function(start, moveStream) {
+        Actions.eraseLines(start);
+
+        var stream = moveStream
+            .withDefault(start)
+            .slidingWindow(2,2);
+
+        this.subscribeTool({
+            stream: stream,
+            onValue: function (line) {
+                Actions.eraseLines(line[1]);
+            }
+        });
+    }
+};
+
+module.exports = ToolsMixin;
